72765c359fe8eebf184bc70696fc371e4f79e0b1
Ale20p <alescode1@gmail.com>
2024-02-01 17:14:16 -0500

    update

--- src/main/java/org/example/StudentObject.java
+++ src/main/java/org/example/StudentObject.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class StudentObject {
     // Meant to be done in a main class
-    Student obj1 = new Student();
+    Student obj1 = new Student("Nick", "Vanier", 18, 7231);
     obj1.age = 19;
     onj1.collegeName = "Vanier";
     obj1.name = "Ale";
